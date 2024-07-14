8d5f7c9daba82c91febe8abd523ef539f26c5701
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-14 12:13:18 -0300

    criação de episode controller e episode service

--- src/controllers/episodesController.ts
+++ src/controllers/episodesController.ts
@@ -1,52 +1,16 @@
 import { Request, Response } from "express";
-import fs from "fs";
-import path from "path";
+import { episodeService } from "../services/episodeService";
 
 export const episodesController = {
   stream: async (req: Request, res: Response) => {
     const { videoUrl } = req.query
+    const range = req.headers.range
 
     try {
       if (typeof videoUrl !== 'string') {
         throw new Error('videoUrl deve ser do tipo \'string\'')
       }
-      const filePath = path.join(__dirname, '../../uploads', videoUrl)
-      const fileStat = fs.statSync(filePath)
-
-      const range = req.headers.range
-
-      if (range) {
-        const parts = range.replace(/bytes=/, '').split('-')
-
-        const start = parseInt(parts[0], 10)
-        const end = parts[1] ? parseInt(parts[1], 10) : fileStat.size - 1
-
-        const chunkSize = (end - start) + 1
-
-        const file = fs.createReadStream(filePath, { start, end })
-
-        const head = {
-          'Content-Range': `bytes ${start}-${end}/${fileStat.size}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': chunkSize,
-          'Content-Type': 'video/mp4',
-        }
-
-        res.writeHead(206, head)
-
-        file.pipe(res)
-      } else {
-
-        const head = {
-          'Content-Length': fileStat.size,
-          'Content-Type': 'video/mp4',
-        }
-
-        res.writeHead(200, head)
-
-        fs.createReadStream(filePath).pipe(res)
-
-      }
+      episodeService.streamEpisodeToResponse(res, videoUrl, range)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
