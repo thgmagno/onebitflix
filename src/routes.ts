0f1821450bd6eecd8c5566dd14309f0ceb11383b
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-16 16:31:44 -0300

    adicionando middleware de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
 import { authController } from './controllers/authController'
+import { ensureAuth } from './middlewares/auth'
+import { episodesController } from './controllers/episodesController'
 
 const router = express.Router()
 
@@ -10,13 +12,16 @@ router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
 // Categories
-router.get('/categories', categoriesController.index)
-router.get('/categories/:id', categoriesController.show)
+router.get('/categories', ensureAuth,  categoriesController.index)
+router.get('/categories/:id', ensureAuth, categoriesController.show)
 
 // Courses
-router.get('/courses/featured', coursesController.featured)
+router.get('/courses/featured', ensureAuth, coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
-router.get('/courses/search', coursesController.search)
-router.get('/courses/:id', coursesController.show)
+router.get('/courses/search', ensureAuth, coursesController.search)
+router.get('/courses/:id', ensureAuth, coursesController.show)
+
+// Episodes
+router.get('/episodes/stream', episodesController.stream)
 
 export { router }
